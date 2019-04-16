052c6e0db4973be59c87dc81259a47af8870fa72
Annia Forney <anniaforney@gmail.com>
2019-04-16 17:22:18 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -7,7 +7,5 @@ end
 #rescue_princess_peach
 #puts status
 
-def rescue_princess_peach
-  status = "rescued"
-  puts "Hooray! Mario has rescued Princess Peach."
-end
+ rescue_princess_peach
+  puts status 
